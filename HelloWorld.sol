e568b273c6df87173716e11996bb463844db872a
sivangbagri <shivangtips@gmail.com>
2024-12-13 16:40:41 +0530

    datatypes

---
+++ HelloWorld.sol
@@ -0,0 +1,12 @@
+ // SPDX-License-Identifier: MIT
+pragma solidity 0.8.26; 
+
+contract HelloWorld{
+    string public myString ="Hello World" ;
+    bool public a =true ;
+    uint public x= 213; // uint === uint256 0 to 2**256 -1
+    int public y = -12; // int === int256 -2**255 to 2**255-1
+    address z;
+    bytes32 public b32;
+
+}
